6aeb6d063f6cd5046c2cc692b17bdef370a7f7d5
stbaeumer <stefan.baeumer@berufskolleg-borken.de>
2023-10-15 07:45:59 +0200

    ...

--- Camt2Smtp/Program.cs
+++ Camt2Smtp/Program.cs
@@ -31,6 +31,7 @@ namespace camt2smtp
             try
             {
                 Console.WriteLine("camt2smtp");
+                Console.WriteLine("Version 20231015" );
                 Console.WriteLine("Published under the terms of GPLv3 Stefan Bäumer 2023.");
                 Console.WriteLine("======================================================");
                 Console.WriteLine("");
